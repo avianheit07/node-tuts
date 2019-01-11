9c47a42c77714971fd5e9e8a131fc327be09ace2
paperchrisp <chris@paperstreetmedia.com>
2019-01-12 07:36:09 +0800

    add redux packages

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+// react, rendering layer
+// components to render on screen
+import React, {Component} from 'react';
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <h1> Heyllo There! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 // import express from 'express';
 
 const app = express();
@@ -9,6 +10,23 @@ app.get('/greeting', (req, res) => {
 app.get('/', (req, res) => {
   res.send('<h1> Welcome to Homepage</h1>');
 });
+// https://rallycoding.herokuapp.com/api/music_albums
+// function fetchAlbum() {
+//   fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+// }
+
+app.get('/async', (req, res) => {
+  fetchAlbum(function(json_obj) {
+    res.send(json_obj);
+  })
+});
+async function fetchAlbum(callback) {
+  const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums')
+  const json = await res.json();
+  callback(json);
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
